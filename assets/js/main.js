54395e6e555390e619c255539b78a96e3761bcb9
Caique Nufe <canufe99@gmail.com>
2023-10-17 17:13:45 -0300

    pokedex

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+const pokemonPopup = document.querySelector('.pokemon-popup');
+const pokemonDetails = document.querySelector('.popup-content'); 
+const closePopup = document.querySelector('.close-button');
+
+
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const maxRecords = 151
+const limit = 10
+let offset = 0;
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}" data-pokemon-name="${pokemon.name}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+
+        const cards = document.querySelectorAll('.pokemon');
+        cards.forEach((card) => {
+            card.addEventListener('click', () => {
+                pokemonPopup.style.display = 'flex';
+                pokemonDetails.innerHTML = card.innerHTML;
+                closePopup.addEventListener('click', () => {
+                    pokemonPopup.style.display = 'none';
+                }) 
+                
+            });
+        });
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
